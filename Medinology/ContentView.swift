77518bf39b610532db2e26871b3e8e3aa9ef32b3
KYHSGeekCode <jourhyang123@gmail.com>
2022-07-19 16:35:39 +0900

    Initial Commit

---
+++ Medinology/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Medinology
+//
+//  Created by 양현서 on 2022/07/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Medinology/MedinologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedinologyApp.swift
+//  Medinology
+//
+//  Created by 양현서 on 2022/07/19.
+//
+
+import SwiftUI
+
+@main
+struct MedinologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
